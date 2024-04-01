691f6aeeeab2d1eddc0c2de900a9a502117dacaf
SkipperV <vlad.tymchuk@gmail.com>
2024-04-01 11:13:59 +0300

    Leave phone number in form after unsuccessful login

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -13,7 +13,8 @@
                     <label for="login" class="inline-block text-lg mb-2">
                         Номер телефону
                     </label>
-                    <input type="text" class="border border-gray-200 rounded p-2 w-full text-black" name="login"/>
+                    <input type="text" class="border border-gray-200 rounded p-2 w-full text-black" name="login"
+                           value="{{old('login')}}"/>
 
                     @error('login')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
