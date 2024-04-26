61e654376ca59dd3ed33f711a97a591f513a6371
SkipperV <vlad.tymchuk@gmail.com>
2024-04-26 16:35:19 +0300

    Add CardController additional checks

--- app/Http/Controllers/Api/CardController.php
+++ app/Http/Controllers/Api/CardController.php
@@ -5,16 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Card;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Response;
 
 class CardController extends Controller
 {
-    public function index(): Collection
+    public function index(): Collection|Response
     {
-        return auth()->user()->cards;
+        $cards = auth()->user()->cards;
+        return $cards->isNotEmpty() ? $cards : response([], 204);
     }
 
     public function show(Card $card): Card
     {
+        if (!auth()->user()->cards()->find($card->id)) {
+            return abort(404);
+        }
         return $card;
     }
 }
