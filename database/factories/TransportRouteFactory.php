b4dfe2008b761c2beb38da81b5dc1878e3a9ac4b
SkipperV <vlad.tymchuk@gmail.com>
2024-11-11 03:00:22 +0200

    refactor: add remaining factories

---
+++ database/factories/TransportRouteFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TransportRoute>
+ */
+class TransportRouteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'city_id' => 1,
+            'route_number' => 1,
+            'transport_type' => "Автобус",
+            'route_endpoint_1' => "Кінцева",
+            'route_endpoint_2' => "Кінцева"
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Card;
+use App\Models\CardTransaction;
+use App\Models\City;
+use App\Models\Ticket;
+use App\Models\TransportRoute;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -14,43 +20,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'login' => 'admin',
             'is_admin' => '1',
             'password' => 'password',
         ]);
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'login' => '+380501234789',
             'password' => 'password',
         ]);
 
-        DB::table('cities')->insert([
+        City::factory()->insert([
             'name' => 'Луцьк',
             'region' => 'Волинська'
         ]);
 
-        DB::table('tickets')->insert([
+        Ticket::factory()->insert([
             'city_id' => 1,
             'transport_type' => "Автобус",
             'ticket_type' => "Стандартний",
             'price' => 14
         ]);
-        DB::table('tickets')->insert([
+        Ticket::factory()->insert([
             'city_id' => 1,
             'transport_type' => "Тролейбус",
             'ticket_type' => "Студентський",
             'price' => 4
         ]);
 
-        DB::table('transport_routes')->insert([
+        TransportRoute::factory()->insert([
             'city_id' => 1,
             'route_number' => 32,
             'transport_type' => "Автобус",
             'route_endpoint_1' => "с. Липини",
             'route_endpoint_2' => "сел. Вересневе",
         ]);
-        DB::table('transport_routes')->insert([
+        TransportRoute::factory()->insert([
             'city_id' => 1,
             'route_number' => 12,
             'transport_type' => "Тролейбус",
@@ -58,16 +63,16 @@ class DatabaseSeeder extends Seeder
             'route_endpoint_2' => "КРЗ",
         ]);
 
-        \App\Models\Card::factory(2)->create();
+        Card::factory(2)->create();
         DB::table('cards')->insert([
             'number' => 11111111111,
             'type' => "Спеціальний",
             'current_balance' => 250
         ]);
 
-        \App\Models\CardTransaction::factory(20)->create();
+        CardTransaction::factory(20)->create();
         for ($i = 1; $i < 4; $i++) {
-            \App\Models\CardTransaction::factory()->create([
+            CardTransaction::factory()->create([
                 'card_id' => $i,
                 'transaction_type' => 1,
                 'balance_change' => fake()->randomElement([100, 150, 200])
@@ -75,7 +80,7 @@ class DatabaseSeeder extends Seeder
         }
 
         for ($i = 1; $i < 100; $i++) {
-            \App\Models\CardTransaction::factory()->create([
+            CardTransaction::factory()->create([
                 'card_id' => 1,
                 'created_at' => date("Y-m-d h:m:s", rand(Carbon::create(2024, 3)->unix(), Carbon::now()->unix()))
             ]);
