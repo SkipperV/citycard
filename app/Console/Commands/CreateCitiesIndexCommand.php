77b1305738fa69a9cb1a0dfd33322f8c2e856888
SkipperV <vlad.tymchuk@gmail.com>
2024-12-18 14:46:21 +0200

    chore: add error logging for creating cities index

--- app/Console/Commands/CreateCitiesIndexCommand.php
+++ app/Console/Commands/CreateCitiesIndexCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Elastic\Elasticsearch\Client;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class CreateCitiesIndexCommand extends Command
 {
@@ -37,35 +38,44 @@ class CreateCitiesIndexCommand extends Command
     {
         $this->info('Creating cities index...');
 
-        $this->elasticsearch->indices()->create([
-            'index' => 'cities',
-            'body' => [
-                'settings' => [
-                    'number_of_replicas' => 0,
-                ],
-                'mappings' => [
-                    'properties' => [
-                        'name' => [
-                            'type' => 'text',
-                            "fields" => [
-                                "keyword" => [
-                                    "type" => "keyword",
+        try {
+            $response = $this->elasticsearch->indices()->create([
+                'index' => 'cities',
+                'body' => [
+                    'settings' => [
+                        'number_of_replicas' => 0,
+                    ],
+                    'mappings' => [
+                        'properties' => [
+                            'name' => [
+                                'type' => 'text',
+                                "fields" => [
+                                    "keyword" => [
+                                        "type" => "keyword",
+                                    ],
                                 ],
                             ],
-                        ],
-                        'region' => [
-                            'type' => 'text',
-                            "fields" => [
-                                "keyword" => [
-                                    "type" => "keyword",
+                            'region' => [
+                                'type' => 'text',
+                                "fields" => [
+                                    "keyword" => [
+                                        "type" => "keyword",
+                                    ],
                                 ],
                             ],
                         ],
                     ],
                 ],
-            ],
-        ]);
+            ]);
 
-        $this->info("\nDone!");
+            if (isset($response['acknowledged']) && $response['acknowledged'] === true) {
+                $this->info("\nDone!");
+            } else {
+                $this->error("\nFailed to create \"cities}\" index.");
+            }
+        } catch (\Exception $e) {
+            Log::channel('elasticsearch')->error("Error creating \"cities\" index: " . $e->getMessage());
+            $this->error("\nError creating \"cities\" index: " . $e->getMessage());
+        }
     }
 }
