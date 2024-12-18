0854b606381bb0597dd415777a077c281741299b
SkipperV <vlad.tymchuk@gmail.com>
2024-12-18 14:32:43 +0200

    refactor: rework reindex command to use db chunks and bulk elasticsearch API

--- app/Console/Commands/ReindexCitiesCommand.php
+++ app/Console/Commands/ReindexCitiesCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\City;
 use Elastic\Elasticsearch\Client;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class ReindexCitiesCommand extends Command
 {
@@ -13,7 +14,7 @@ class ReindexCitiesCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'search:reindex-cities';
+    protected $signature = 'search:reindex:cities';
 
     /**
      * The console command description.
@@ -21,6 +22,7 @@ class ReindexCitiesCommand extends Command
      * @var string
      */
     protected $description = 'Indexes all cities to Elasticsearch';
+
     private $elasticsearch;
 
     public function __construct(Client $elasticsearch)
@@ -35,16 +37,46 @@ class ReindexCitiesCommand extends Command
      */
     public function handle()
     {
-        $this->info('Indexing all cities. This might take a while...');
+        $errorsLogged = false;
 
-        foreach (City::cursor() as $city) {
-            $this->elasticsearch->index([
-                'index' => $city->getSearchIndex(),
-                'id' => $city->getKey(),
-                'body' => $city->toSearchArray(),
-            ]);
+        $this->info("Indexing all cities. This might take a while...");
+
+        City::query()->orderBy('id')->chunk(500, function ($cities) use (&$errorsLogged) {
+            $response = $this->reindexCities('cities', $cities->toArray());
+
+            if (!empty($response['errors'])) {
+                foreach ($response['items'] as $item) {
+                    if (isset($item['index']['error'])) {
+                        Log::channel('elasticsearch')->error('Error during Elasticsearch indexing.', [
+                            'error' => $item['index']['error'],
+                            'item' => $item,
+                        ]);
+                        $errorsLogged = true;
+                    }
+                }
+            }
+        });
+
+        if ($errorsLogged) {
+            $this->info('Some errors occurred during reindexing. Please check the "elasticsearch.log" for more details.');
+        }
+        $this->info("\nDone!");
+    }
+
+    public function reindexCities(string $index, array $cities)
+    {
+        $bulkData = [];
+
+        foreach ($cities as $city) {
+            $bulkData[] = [
+                'index' => [
+                    '_index' => $index,
+                    '_id' => $city['id'],
+                ]
+            ];
+            $bulkData[] = $city;
         }
 
-        $this->info("\nDone!");
+        return $this->elasticsearch->bulk(['body' => $bulkData]);
     }
 }
