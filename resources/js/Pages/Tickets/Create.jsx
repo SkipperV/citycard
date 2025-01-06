9296f6022511e77cd9c51b6aea2c134e69128a9a
SkipperV <vlad.tymchuk@gmail.com>
2025-01-06 10:23:40 +0200

    refactor: mutation (creation and editing) of cities, transport and tickets data using react-query

--- resources/js/Pages/Tickets/Create.jsx
+++ resources/js/Pages/Tickets/Create.jsx
@@ -3,22 +3,41 @@ import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import {Head, router, useForm} from '@inertiajs/react';
+import {Head, Link, router} from '@inertiajs/react';
 import SelectInput from "@/Components/SelectInput.jsx";
 import {useTranslation} from "react-i18next";
+import {useState} from "react";
+import {useMutation} from "@tanstack/react-query";
 
 export default function Create({auth, status, city}) {
-    const {t} = useTranslation()
+    const {t} = useTranslation();
 
-    const handleRedirect = () => {
-        router.visit(route('tickets.index', city.id));
-    }
+    const [transportType, setTransportType] = useState("");
+    const [ticketType, setTicketType] = useState("");
+    const [price, setPrice] = useState("");
+    const [errors, setErrors] = useState({});
 
-    const {data, setData, post, processing, errors} = useForm({
-        transport_type: '',
-        ticket_type: '',
-        price: '',
+    const mutation = useMutation({
+        mutationFn: async ({transportType, ticketType, price}) => {
+            return await axios.post(route('api.tickets.store', {city: city.id}), {
+                transport_type: transportType,
+                ticket_type: ticketType,
+                price: price
+            });
+        },
+        onSuccess: () => {
+            setErrors({});
+            router.visit(route('tickets.index', {city: city.id}));
+        },
+        onError: (error) => {
+            setErrors(error.response.data.errors);
+        }
     });
+
+    const submit = (e) => {
+        e.preventDefault();
+        mutation.mutate({transportType, ticketType, price});
+    };
 
     const transportTypeOptions = [
         'bus',
@@ -33,26 +52,21 @@ export default function Create({auth, status, city}) {
         'special',
     ];
 
-    const submit = (e) => {
-        e.preventDefault();
-
-        post(route('tickets.store', city.id));
-    };
-
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={
                 <div className="relative w-full h-6">
-                    <svg onClick={handleRedirect}
-                         className="absolute left-3 top-1 h-4 text-gray-800 hover:cursor-pointer dark:text-white"
-                         aria-hidden="true"
-                         xmlns="http://www.w3.org/2000/svg"
-                         fill="none"
-                         viewBox="0 0 8 14">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
-                    </svg>
+                    <Link href={route('tickets.index', {city: city.id})}>
+                        <svg className="absolute left-3 top-1 h-4 text-gray-800 hover:cursor-pointer dark:text-white"
+                             aria-hidden="true"
+                             xmlns="http://www.w3.org/2000/svg"
+                             fill="none"
+                             viewBox="0 0 8 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
+                        </svg>
+                    </Link>
                     <h2 className="absolute left-1/2 transform -translate-x-1/2 top-0 h-full font-semibold text-xl text-gray-800 leading-tight dark:text-gray-300 text-center">
                         {t("tickets.title.create")} {city.name}
                     </h2>
@@ -73,11 +87,14 @@ export default function Create({auth, status, city}) {
                                 className="mt-1 block w-full"
                                 options={transportTypeOptions}
                                 object="transport"
-                                value={data.transport_type}
-                                onChange={(e) => setData('transport_type', e.target.value)}
+                                value={transportType}
+                                onChange={(e) => setTransportType(e.target.value)}
                             />
-
-                            <InputError message={errors.transport_type} className="mt-2"/>
+                            {
+                                errors.transport_type && errors.transport_type.map((err, index) => (
+                                    <InputError key={index} message={err} className="mt-2"/>
+                                ))
+                            }
                         </div>
 
                         <div className="mt-4">
@@ -88,11 +105,14 @@ export default function Create({auth, status, city}) {
                                 className="mt-1 block w-full"
                                 options={ticketTypeOptions}
                                 object="tickets"
-                                value={data.ticket_type}
-                                onChange={(e) => setData('ticket_type', e.target.value)}
+                                value={ticketType}
+                                onChange={(e) => setTicketType(e.target.value)}
                             />
-
-                            <InputError message={errors.ticket_type} className="mt-2"/>
+                            {
+                                errors.ticket_type && errors.ticket_type.map((err, index) => (
+                                    <InputError key={index} message={err} className="mt-2"/>
+                                ))
+                            }
                         </div>
 
                         <div className="mt-4">
@@ -102,16 +122,19 @@ export default function Create({auth, status, city}) {
                                 id="price"
                                 type="text"
                                 name="price"
-                                value={data.price}
+                                value={price}
                                 className="mt-1 block w-full"
-                                onChange={(e) => setData('price', e.target.value)}
+                                onChange={(e) => setPrice(e.target.value)}
                             />
-
-                            <InputError message={errors.price} className="mt-2"/>
+                            {
+                                errors.price && errors.price.map((err, index) => (
+                                    <InputError key={index} message={err} className="mt-2"/>
+                                ))
+                            }
                         </div>
 
                         <div className="mt-4 flex">
-                            <PrimaryButton className="mx-auto" disabled={processing}>
+                            <PrimaryButton className="mx-auto" disabled={mutation.isPending}>
                                 {t("operations.create")}
                             </PrimaryButton>
                         </div>
--- resources/js/Pages/Tickets/Edit.jsx
+++ resources/js/Pages/Tickets/Edit.jsx
@@ -3,22 +3,41 @@ import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import {Head, router, useForm} from '@inertiajs/react';
+import {Head, Link, router} from '@inertiajs/react';
 import SelectInput from "@/Components/SelectInput.jsx";
 import {useTranslation} from "react-i18next";
+import {useState} from "react";
+import {useMutation} from "@tanstack/react-query";
 
 export default function Edit({auth, status, city, ticket}) {
-    const {t} = useTranslation()
+    const {t} = useTranslation();
 
-    const handleRedirect = () => {
-        router.visit(route('tickets.index', city.id));
-    }
+    const [transportType, setTransportType] = useState(ticket.transport_type);
+    const [ticketType, setTicketType] = useState(ticket.ticket_type);
+    const [price, setPrice] = useState(ticket.price);
+    const [errors, setErrors] = useState({});
 
-    const {data, setData, put, processing, errors} = useForm({
-        transport_type: ticket.transport_type,
-        ticket_type: ticket.ticket_type,
-        price: ticket.price,
+    const mutation = useMutation({
+        mutationFn: async ({transportType, ticketType, price}) => {
+            return await axios.put(route('api.tickets.update', {city: city.id, ticket: ticket.id}), {
+                transport_type: transportType,
+                ticket_type: ticketType,
+                price: price
+            });
+        },
+        onSuccess: () => {
+            setErrors({});
+            router.visit(route('tickets.index', {city: city.id}));
+        },
+        onError: (error) => {
+            setErrors(error.response.data.errors);
+        }
     });
+
+    const submit = (e) => {
+        e.preventDefault();
+        mutation.mutate({transportType, ticketType, price});
+    };
 
     const transportTypeOptions = [
         'bus',
@@ -33,26 +52,21 @@ export default function Edit({auth, status, city, ticket}) {
         'special',
     ];
 
-    const submit = (e) => {
-        e.preventDefault();
-
-        put(route('tickets.update', [city.id, ticket.id]));
-    };
-
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={
                 <div className="relative w-full h-6">
-                    <svg onClick={handleRedirect}
-                         className="absolute left-3 top-1 h-4 text-gray-800 hover:cursor-pointer dark:text-white"
-                         aria-hidden="true"
-                         xmlns="http://www.w3.org/2000/svg"
-                         fill="none"
-                         viewBox="0 0 8 14">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
-                    </svg>
+                    <Link href={route('tickets.index', {city: city.id})}>
+                        <svg className="absolute left-3 top-1 h-4 text-gray-800 hover:cursor-pointer dark:text-white"
+                             aria-hidden="true"
+                             xmlns="http://www.w3.org/2000/svg"
+                             fill="none"
+                             viewBox="0 0 8 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
+                        </svg>
+                    </Link>
                     <h2 className="absolute left-1/2 transform -translate-x-1/2 top-0 h-full font-semibold text-xl text-gray-800 leading-tight dark:text-gray-300 text-center">
                         {t("tickets.title.update")}
                     </h2>
@@ -73,11 +87,14 @@ export default function Edit({auth, status, city, ticket}) {
                                 className="mt-1 block w-full"
                                 options={transportTypeOptions}
                                 object="transport"
-                                value={data.transport_type}
-                                onChange={(e) => setData('transport_type', e.target.value)}
+                                value={transportType}
+                                onChange={(e) => setTransportType(e.target.value)}
                             />
-
-                            <InputError message={errors.transport_type} className="mt-2"/>
+                            {
+                                errors.transport_type && errors.transport_type.map((err, index) => (
+                                    <InputError key={index} message={err} className="mt-2"/>
+                                ))
+                            }
                         </div>
 
                         <div className="mt-4">
@@ -88,11 +105,14 @@ export default function Edit({auth, status, city, ticket}) {
                                 className="mt-1 block w-full"
                                 options={ticketTypeOptions}
                                 object="tickets"
-                                value={data.ticket_type}
-                                onChange={(e) => setData('ticket_type', e.target.value)}
+                                value={ticketType}
+                                onChange={(e) => setTicketType(e.target.value)}
                             />
-
-                            <InputError message={errors.ticket_type} className="mt-2"/>
+                            {
+                                errors.ticket_type && errors.ticket_type.map((err, index) => (
+                                    <InputError key={index} message={err} className="mt-2"/>
+                                ))
+                            }
                         </div>
 
                         <div className="mt-4">
@@ -102,16 +122,19 @@ export default function Edit({auth, status, city, ticket}) {
                                 id="price"
                                 type="text"
                                 name="price"
-                                value={data.price}
+                                value={price}
                                 className="mt-1 block w-full"
-                                onChange={(e) => setData('price', e.target.value)}
+                                onChange={(e) => setPrice(e.target.value)}
                             />
-
-                            <InputError message={errors.price} className="mt-2"/>
+                            {
+                                errors.price && errors.price.map((err, index) => (
+                                    <InputError key={index} message={err} className="mt-2"/>
+                                ))
+                            }
                         </div>
 
                         <div className="mt-4 flex">
-                            <PrimaryButton className="mx-auto" disabled={processing}>
+                            <PrimaryButton className="mx-auto" disabled={mutation.isPending}>
                                 {t(`operations.save`)}
                             </PrimaryButton>
                         </div>
